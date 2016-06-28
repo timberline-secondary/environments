4b16aceb1f5852ab3f305a090083078989e3c852
Hackerspace Admin <timberline.hackerspace@gmail.com>
2016-06-28 14:37:20 -0700

    python and unity3d

--- production/modules/hs_developer_tools/manifests/python.pp
+++ production/modules/hs_developer_tools/manifests/python.pp
@@ -3,6 +3,4 @@ class hs_developer_tools::python {
 	package { 'idle3':
 		ensure  => latest,
 	}
-
-
 }
---
+++ production/modules/hs_developer_tools/manifests/unity3d.pp
@@ -0,0 +1,28 @@
+class hs_developer_tools::unity3d {
+
+	# Latest Unity build for Linux
+	# http://forum.unity3d.com/threads/unity-on-linux-release-notes-and-known-issues.350256/
+
+	file { '/opt/unity3d': 
+		ensure	=> directory,
+	}
+
+	file { 'Unity3D Deb':
+		path	=> '/opt/unity3d/unity-editor-5.4.0b21+20160609_amd64.deb',
+		owner	=> root,
+		group	=> root,
+		mode	=> '644',
+		ensure	=> present,
+		source	=> 'puppet:///modules/hs_developer_tools/unity-editor-5.4.0b21+20160609_amd64.deb',
+		require => File['/opt/unity3d'],
+	}
+
+	package { 'unity-editor': 
+		provider => dpkg,
+		ensure	=> latest,
+		source => '/opt/unity3d/unity-editor-5.4.0b21+20160609_amd64.deb',
+		require => File['Unity3D Deb'],
+	}
+
+
+}
