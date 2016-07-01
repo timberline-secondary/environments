d756d10e9dacd2e8dd46de1b33fb4dc3b853c49f
tylerecouture <tylerecouture@users.noreply.github.com>
2016-06-30 19:38:59 -0700

    Update site.pp

--- production/manifests/site.pp
+++ production/manifests/site.pp
@@ -15,5 +15,5 @@ node default {
 	class { 'hs_developer_tools': }
 	class { 'hs_developer_tools::eclipse': }
 	class { 'hs_developer_tools::python': }
-	#class { 'hs_developer_tools::unity3d': }
+	class { 'hs_developer_tools::unity3d': }
 }
