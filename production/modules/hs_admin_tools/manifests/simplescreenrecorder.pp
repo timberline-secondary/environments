83961b7fe3ca5c7b9d55f2437229100dd61d13bd
tylerecouture <tylerecouture@users.noreply.github.com>
2016-09-03 13:58:42 -0700

    Update simplescreenrecorder.pp

--- production/modules/hs_admin_tools/manifests/simplescreenrecorder.pp
+++ production/modules/hs_admin_tools/manifests/simplescreenrecorder.pp
@@ -1,1 +1,8 @@
 
+apt::ppa { 'ppa:maarten-baert/simplescreenrecorder': 
+    ensure => present,
+}
+package { 'simplescreenrecorder':
+    ensure  => latest,
+    require => [ Class['apt::update'], Apt::Ppa['ppa:maarten-baert/simplescreenrecorder'] ],
+}
