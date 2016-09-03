130919180a4adf14e84172a1114351badae72d75
tylerecouture <tylerecouture@users.noreply.github.com>
2016-09-03 16:59:33 -0700

    Update unity3d.pp

--- production/modules/hs_developer_tools/manifests/unity3d.pp
+++ production/modules/hs_developer_tools/manifests/unity3d.pp
@@ -12,19 +12,19 @@ class hs_developer_tools::unity3d {
 	}
 
 	file { 'unity-editor-deb':
-		path	=> '/opt/unity3d/unity-editor-5.4.0b21+20160609_amd64.deb',
+		path	=> '/opt/unity3d/unity-editor-5.4.0p1+20160810_amd64.deb',
 		owner	=> root,
 		group	=> root,
 		mode	=> '644',
 		ensure	=> present,
-		source	=> 'puppet:///modules/hs_developer_tools/unity-editor-5.4.0b21+20160609_amd64.deb',
+		source	=> 'puppet:///modules/hs_developer_tools/unity-editor-5.4.0p1+20160810_amd64.deb',
 		require => File['/opt/unity3d'],
 	}
 
 	package { 'unity-editor': 
 		provider => dpkg,
 		ensure	=> latest,
-		source => '/opt/unity3d/unity-editor-5.4.0b21+20160609_amd64.deb',
+		source => '/opt/unity3d/unity-editor-5.4.0p1+20160810_amd64.deb',
 		require => File['unity-editor-deb'],
 	}
 
