4f7d0dfc2092dee78b6b0cb77dadd4a01fb4dfae
tylerecouture <tylerecouture@users.noreply.github.com>
2016-09-06 23:10:05 -0700

    Update kaku.pp

--- production/modules/hs_student_apps/manifests/kaku.pp
+++ production/modules/hs_student_apps/manifests/kaku.pp
@@ -1,29 +1,4 @@
-file { 'nsswitch.conf':
-    path    => '/etc/nsswitch.conf',
-    ensure  => file,
-    require => Package['libnss-ldap'],
-    source  => "puppet:///modules/hs_ldap_client/nsswitch.conf",
-    #notify  => Reboot['after_run'],
-    # This file is in: modules/hs_ldap_client/files
-} ->
+# Use a puppet download and extract module: http://kaku.rocks/
 
+# then copy over .desktop file and icon: https://help.ubuntu.com/community/UnityLaunchersAndDesktopFiles
 
-file { '/devops/RRDI-repo-Linux64-5.0.2GA.zip':
-  source => 'puppet:///somemodule//devops/RRDI-repo-Linux64-5.0.2GA.zip'
-  notify => Exec['unzip'],
-}
-
-file { '/tmp/RRDI_INSTALLATION/':
-  ensure => 'directory',
-  mode   => '1777',
-  owner  => 'user',
-  group  => 'user',
-}
-
-exec { 'unzip':
-  command     => 'unzip /devops/RRDI-repo-Linux64-5.0.2GA.zip -d /tmp/RRDI_INSTALLATION/',
-  cwd         => '/home/user/',
-  user        => 'root',
-  require     => File["/tmp/RRDI_INSTALLATION/"],
-  refreshonly => true,
-}
