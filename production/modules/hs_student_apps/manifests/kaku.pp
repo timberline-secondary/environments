ec09419e395b02437839656af4b50c265383e862
tylerecouture <tylerecouture@users.noreply.github.com>
2016-09-06 22:32:01 -0700

    Update kaku.pp

--- production/modules/hs_student_apps/manifests/kaku.pp
+++ production/modules/hs_student_apps/manifests/kaku.pp
@@ -1,1 +1,20 @@
 
+file { '/devops/RRDI-repo-Linux64-5.0.2GA.zip':
+  source => 'puppet:///somemodule//devops/RRDI-repo-Linux64-5.0.2GA.zip'
+  notify => Exec['unzip'],
+}
+
+file { '/tmp/RRDI_INSTALLATION/':
+  ensure => 'directory',
+  mode   => '1777',
+  owner  => 'user',
+  group  => 'user',
+}
+
+exec { 'unzip':
+  command     => 'unzip /devops/RRDI-repo-Linux64-5.0.2GA.zip -d /tmp/RRDI_INSTALLATION/',
+  cwd         => '/home/user/',
+  user        => 'root',
+  require     => File["/tmp/RRDI_INSTALLATION/"],
+  refreshonly => true,
+}
