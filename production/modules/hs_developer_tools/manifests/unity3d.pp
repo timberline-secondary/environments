515f642b3bb4228c0e98cffc4d2825445e61c6cf
tylerecouture <tylerecouture@users.noreply.github.com>
2016-09-11 12:31:50 -0700

    gdebi

--- production/modules/hs_developer_tools/manifests/unity3d.pp
+++ production/modules/hs_developer_tools/manifests/unity3d.pp
@@ -23,8 +23,9 @@ class hs_developer_tools::unity3d {
 		require => File['/opt/unity3d'],
 	}
 	
+	include gdebi
 	package { 'unity-editor': 
-		provider => dpkg,
+		provider => gdebi,
 		ensure	=> latest,
 		source => '/opt/unity3d/unity-editor-5.5.0b2+20160906_amd64.deb',
 		require => File['unity-editor-deb'],
