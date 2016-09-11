b4de93fef35d1c23737787ce53a18399579cdf4a
tylerecouture <tylerecouture@users.noreply.github.com>
2016-09-10 20:41:43 -0700

    Update motd.pp

--- production/modules/helloworld/manifests/motd.pp
+++ production/modules/helloworld/manifests/motd.pp
@@ -1,8 +1,10 @@
+$timestamp = generate('/bin/date', '+%Y%d%m_%H:%M:%S')
+
 class helloworld::motd {
 	file { '/etc/motd':
 	owner => 'root',
 	group => 'root',
 	mode => '0644',
-	content => "Production puppetmaster is in control.\n",
+	content => "Production puppetmaster is in control. Last run: ${timestamp}\n",
 	}
 }
