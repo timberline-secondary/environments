d42c2a48f2da9e494f85d23bbd33b30650650af2
tylerecouture <tylerecouture@users.noreply.github.com>
2016-09-12 16:43:27 -0700

    Update init.pp

--- production/modules/hs_utilities/manifests/init.pp
+++ production/modules/hs_utilities/manifests/init.pp
@@ -3,9 +3,18 @@ class hs_utilities {
 
   notify { 'Puppet Module - Utilities': }
 
-	package { 'wine':
-		ensure  => latest,
+  package { 'wine':
+    ensure  => latest,
   }
+  
+  # Screen gif capturing
+  apt::ppa { 'ppa:sethj/silentcast': 
+      ensure => present,
+  }
+  package { 'silentcast':
+      ensure  => latest,
+      require => [ Class['apt::update'], Apt::Ppa['ppa:sethj/silentcast'] ],
+  }  
   
   
 }
