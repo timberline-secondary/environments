2da4ac88ceb235102c447239705b99b83172b0d1
tylerecouture <tylerecouture@users.noreply.github.com>
2016-09-13 17:38:31 -0700

    Update kaku.pp

--- production/modules/hs_student_apps/manifests/kaku.pp
+++ production/modules/hs_student_apps/manifests/kaku.pp
@@ -2,11 +2,18 @@ class hs_student_apps::kaku {
 
 	file { 'Kaku-linux64.zip':
 		path	=> '/usr/local/bin/Kaku-linux64.zip',
-		owner	=> root,
-		group	=> root,
-		mode	=> '644',
-		ensure	=> present,
+		ensure	=> absent,
 		source	=> 'puppet:///modules/hs_student_apps/kaku/Kaku-linux64.zip',
+	}
+
+	archive { 'Kaku-linux64':
+	   ensure => present,
+	   url => 'https://github.com/EragonJ/Kaku/releases/download/1.7.0/Kaku-linux64.zip',
+	   target => '/usr/local/bin',
+	   follow_redirects => true,
+	   extension => 'zip',
+	   checksum => false,
+	   src_target => '/tmp'
 	}
 	
 	file { 'Kaku.png':
