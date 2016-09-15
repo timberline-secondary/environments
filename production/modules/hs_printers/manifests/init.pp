8c2edb1ac6a89bdf223597e73ad54e8d28a814f5
tylerecouture <tylerecouture@users.noreply.github.com>
2016-09-15 14:13:21 -0700

    Update init.pp

--- production/modules/hs_printers/manifests/init.pp
+++ production/modules/hs_printers/manifests/init.pp
@@ -1,9 +1,11 @@
 class hs_printers {
 
-  file { 'printers.conf':
+  #Overwrites color printer for teachers.  
+  #Need to figure out how to add color printer to teacher machine without adding to student machine>
+  /*file { 'printers.conf':
     path    => '/etc/cups/printers.conf',
     ensure  => file,
     source  => "puppet:///modules/hs_printers/printers.conf",
-  }
+  }*/
 
 }
