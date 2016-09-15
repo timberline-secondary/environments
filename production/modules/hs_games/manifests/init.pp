6deae8a4945cd290dae66afa9ad55a1b7029d4cd
tylerecouture <tylerecouture@users.noreply.github.com>
2016-09-15 16:04:13 -0700

    Update init.pp

--- production/modules/hs_games/manifests/init.pp
+++ production/modules/hs_games/manifests/init.pp
@@ -1,10 +1,28 @@
-class hs_games: {
+class hs_games {
 
-  package { 'inkscape':
+  notify { 'Puppet Module - Games': }
+
+  package { 'steam':
       ensure  => latest,
-      require => [ Class['apt::update'], Apt::Ppa['ppa:inkscape.dev/stable'] ],
   }
-
+  
+	/*file { 'unity-editor-deb':
+		path	=> '/opt/unity3d/unity-editor-5.5.0b2+20160906_amd64.deb',
+		owner	=> root,
+		group	=> root,
+		mode	=> '644',
+		ensure	=> present,
+		source	=> 'puppet:///modules/hs_developer_tools/unity-editor-5.5.0b2+20160906_amd64.deb',
+		require => File['/opt/unity3d'],
+	}*/
+	
+	/*include gdebi
+	package { 'unity-editor': 
+		provider => gdebi,
+		ensure	=> latest,
+		source => '/opt/unity3d/unity-editor-5.5.0b2+20160906_amd64.deb',
+		require => File['unity-editor-deb'],
+  }*/
 
 
 }
