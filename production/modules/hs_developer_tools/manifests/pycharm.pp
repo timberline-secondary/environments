79511d06e14b0d3c1e6c843cffe212835a10803e
tylerecouture <tylerecouture@users.noreply.github.com>
2016-09-18 14:48:49 -0700

    Update pycharm.pp

--- production/modules/hs_developer_tools/manifests/pycharm.pp
+++ production/modules/hs_developer_tools/manifests/pycharm.pp
@@ -1,1 +1,14 @@
 
+class hs_developer_tools::pycharm {
+	
+  apt::ppa { 'ppa:mystic-mirage/pycharm': 
+      ensure => present,
+  }
+  package { 'pycharm':
+      ensure  => latest,
+      require => [ Class['apt::update'], Apt::Ppa['ppa:mystic-mirage/pycharm'] ],
+  }
+
+}
+ 
+ 
