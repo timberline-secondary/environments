d06429c8d8a343a66e245afc97daa58ed35c78e9
tylerecouture <tylerecouture@users.noreply.github.com>
2016-10-03 16:05:27 -0700

    Update init.pp

--- production/modules/hs_apt/manifests/init.pp
+++ production/modules/hs_apt/manifests/init.pp
@@ -12,10 +12,16 @@ class hs_apt {
 
 # Do this: https://www.memonic.com/user/pneff/folder/55756627-f51c-43f0-adfd-777635574108/id/1Z9999x
 
-        exec { "upgrade":
-                command => "apt full-upgrade",
-                path => "/usr/bin/",
-        }
+#        exec { "upgrade":
+#                command => "apt full-upgrade",
+#                path => "/usr/bin/",
+#        }
 
+  cron { 'cron-upgrade':
+    command => '/usr/bin/apt full-upgrade',
+    user    => 'root',
+    hour    => 16,
+    minute  => 7,
+  }
 
 }
