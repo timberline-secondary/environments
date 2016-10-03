e8a68237d96627b1753b0365bd4de666428e19d6
tylerecouture <tylerecouture@users.noreply.github.com>
2016-10-03 15:48:06 -0700

    Update init.pp

--- production/modules/hs_apt/manifests/init.pp
+++ production/modules/hs_apt/manifests/init.pp
@@ -1,1 +1,21 @@
 
+class hs_apt {
+
+  # https://forge.puppet.com/puppetlabs/apt
+  include apt
+
+#  class { 'apt':
+#    update => {
+#      frequency => 'daily',
+#    },
+#  }
+
+# Do this: https://www.memonic.com/user/pneff/folder/55756627-f51c-43f0-adfd-777635574108/id/1Z9999x
+
+        exec { "upgrade":
+                command => "apt-get upgrade -y",
+                path => "/usr/bin/",
+        }
+
+
+}
