360965f8d886772e32978cba7e52c01ec717a5ef
tylerecouture <tylerecouture@users.noreply.github.com>
2016-10-06 14:33:17 -0700

    Update init.pp

--- production/modules/hs_apt/manifests/init.pp
+++ production/modules/hs_apt/manifests/init.pp
@@ -11,6 +11,17 @@ class hs_apt {
               'reboot_time' => '4am', },
   }
 
+  file { "/etc/update_initiator":
+    source => "puppet://puppet/hs_apt/update_initiator",
+  }
+
+  exec { "/usr/bin/apt-get -y dist-upgrade":
+    refreshonly => true,
+    subscribe => File["/etc/update_initiator"],
+  }
+
+
+
 #  class { 'apt':
 #    update => {
 #      frequency => 'daily',
@@ -18,10 +29,10 @@ class hs_apt {
 #  }
 
 # Do this: https://www.memonic.com/user/pneff/folder/55756627-f51c-43f0-adfd-777635574108/id/1Z9999x
-  exec { "upgrade":
-    command => "apt-get -y dist-upgrade",
-    path => "/usr/bin/",
-  }
+#  exec { "upgrade":
+#    command => "apt-get -y dist-upgrade",
+#    path => "/usr/bin/",
+#  }
 
   cron { 'cron-upgrade':
     command => '/usr/bin/apt -y full-upgrade',
