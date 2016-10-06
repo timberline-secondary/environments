e0df764e2933adee0efec4e09b255c358bc04784
tylerecouture <tylerecouture@users.noreply.github.com>
2016-10-06 14:00:45 -0700

    Update init.pp

--- production/modules/hs_apt/manifests/init.pp
+++ production/modules/hs_apt/manifests/init.pp
@@ -7,7 +7,7 @@ class hs_apt {
   # include unattended_upgrades
   
   class { 'unattended_upgrades':
-    auto => { 'reboot' => true,
+    auto => { 'reboot' => false,
               'reboot_time' => '4am', },
   }
 
@@ -18,11 +18,10 @@ class hs_apt {
 #  }
 
 # Do this: https://www.memonic.com/user/pneff/folder/55756627-f51c-43f0-adfd-777635574108/id/1Z9999x
-
-#        exec { "upgrade":
-#                command => "apt full-upgrade",
-#                path => "/usr/bin/",
-#        }
+  exec { "upgrade":
+    command => "apt-get -y dist-upgrade",
+    path => "/usr/bin/",
+  }
 
   cron { 'cron-upgrade':
     command => '/usr/bin/apt -y full-upgrade',
