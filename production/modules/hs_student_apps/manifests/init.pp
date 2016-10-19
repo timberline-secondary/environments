23f9205a4e42dee8b1f91460ccafb1d3001b6bae
tylerecouture <tylerecouture@users.noreply.github.com>
2016-10-19 09:25:24 -0700

    Update init.pp

--- production/modules/hs_student_apps/manifests/init.pp
+++ production/modules/hs_student_apps/manifests/init.pp
@@ -18,45 +18,6 @@ class hs_student_apps {
   package { 'musescore':  }
   package { 'chromium-browser':   }
 
-
-  #############
-  #
-  # Blender
-  #
-  #############
-
-  apt::ppa { 'ppa:thomas-schiex/blender': 
-      ensure => present,
-  }
-  package { 'blender':
-      ensure  => '2.78~release~1475052116-0thomas~xenial1',
-      require => [ Class['apt::update'], Apt::Ppa['ppa:thomas-schiex/blender'] ],
-  }
-  
-  # Overwrite netrender addon to allow for use of GPU on slaves
-  # https://github.com/WARP-LAB/Blender-Network-Render-Additions
-  
-  # fully managed directory: https://christian.hofstaedtler.name/blog/2008/11/puppet-managing-directories-recursively.html
-  file { 'blender-netrender-addon':
-    path    => '/usr/share/blender/2.78/scripts/addons/netrender',
-    recurse => true,
-    purge   => true,
-    force   => true,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    ensure  => directory,
-    require => Package['blender'],
-    source  =>
-  } 
-  
- # file { 'lightdm.conf':
- #   path    => '/etc/lightdm/lightdm.conf',
- #   ensure  => file,
- #   source  => "https://github.com/WARP-LAB/Blender-Network-Render-Additions.git",
- #   require => File['/etc/lightdm'],
- # }
-
   #############
   #  
   # GIMP
