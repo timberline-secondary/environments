815e0c6bdad83f1a8f62feaeb07d35c48aef372e
tylerecouture <tylerecouture@users.noreply.github.com>
2017-01-16 15:47:45 -0800

    Update atom.pp

--- production/modules/hs_developer_tools/manifests/atom.pp
+++ production/modules/hs_developer_tools/manifests/atom.pp
@@ -1,21 +1,11 @@
-	file { '/opt/appinventor': 
-		ensure	=> directory,
-	}
-
-	file { 'app-inventor-2-deb':
-		path	=> '/opt/appinventor/appinventor2-setup_2.3_all.deb',
-		owner	=> root,
-		group	=> root,
-		mode	=> '644',
-		ensure	=> present,
-		source	=> 'puppet:///modules/hs_developer_tools/appinventor2-setup_2.3_all.deb',
-		require => File['/opt/appinventor'],
-	}
+class hs_developer_tools::atom {
 	
-	include gdebi
-	package { 'app-inventer-2': 
-		provider => gdebi,
-		ensure	=> latest,
-		source => '/opt/appinventor/appinventor2-setup_2.3_all.deb',
-		require => File['app-inventor-2-deb'],
+  apt::ppa { 'ppa:webupd8team/atom': 
+      ensure => present,
+  }
+  package { 'atom':
+      ensure  => latest,
+      require => [ Class['apt::update'], Apt::Ppa['ppa:webupd8team/atom'] ],
+  }
+
 }
