345ca620e48a91e7aee9ed35f70097db95e7c609
tylerecouture <tylerecouture@gmail.com>
2017-02-03 17:30:12 -0800

    blender ppa

--- production/modules/hs_student_apps/manifests/blender.pp
+++ production/modules/hs_student_apps/manifests/blender.pp
@@ -2,13 +2,22 @@ class hs_student_apps::blender {
 
   include apt
   
-  apt::ppa { 'ppa:thomas-schiex/blender': 
-    ensure => 'absent',
-    options => '--remove'
-  }
+  # apt::ppa { 'ppa:thomas-schiex/blender':
+  #   ensure => 'absent',
+  #   options => '--remove'
+  # }
   package { 'blender':
     ensure  => present,
     # require => [ Class['apt::update'], Apt::Ppa['ppa:thomas-schiex/blender'] ],
+  }
+
+  file { 'thomas-schiex/blender':
+    path    => '/etc/apt/sources.list.d/thomas-schiex-ubuntu-blender-xenial.list',
+    ensure  => absent,
+  }
+  file { 'thomas-schiex/blender2':
+    path    => '/etc/apt/sources.list.d/thomas-schiex-ubuntu-blender-xenial.list.save',
+    ensure  => absent,
   }
   
   # Overwrite netrender addon to allow for use of GPU on slaves
