7f2464c7ffa6300603563c91d29ecd0d0c535ea0
tylerecouture <tylerecouture@gmail.com>
2017-03-08 16:11:00 -0800

    remove printers

--- production/modules/hs_printers/manifests/init.pp
+++ production/modules/hs_printers/manifests/init.pp
@@ -2,10 +2,10 @@ class hs_printers {
 
   #Overwrites color printer for teachers.  
   #Need to figure out how to add color printer to teacher machine without adding to student machine>
-  /*file { 'printers.conf':
+  file { 'printers.conf':
     path    => '/etc/cups/printers.conf',
-    ensure  => file,
+    ensure  => absent,
     source  => "puppet:///modules/hs_printers/printers.conf",
-  }*/
+  }
 
 }
