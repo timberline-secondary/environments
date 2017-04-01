934db1d9b49516848f9f9a27ef4e84fc922e0473
tylerecouture <tylerecouture@gmail.com>
2017-03-31 21:54:49 -0700

    printer T644 cups

--- production/modules/hs_printers/manifests/init.pp
+++ production/modules/hs_printers/manifests/init.pp
@@ -8,8 +8,32 @@ class hs_printers {
   #   source  => "puppet:///modules/hs_printers/printers.conf",
   # }
 
+  # https://forge.puppet.com/leoarnold/cups#option-defaults
   include '::cups'
 
+  cups_queue { 'Hackerspace B&W Printer (Lexmark T644 - Minimal)':
+    ensure => 'printer',
+    model  => 'foomatic-db-compressed-ppds:0/ppd/foomatic-ppd/Lexmark-T644-Postscript.ppd',
+    uri    => 'ipp://192.168.43.101', # Replace with your printer's URI
+    accepting => true,
+    enabled => true,
+
+  }
+
+
+  # https://lauri.võsandi.com/cfgmgmt/puppet-cups.html
+  # Install CUPS and filters
+  package { "system-config-printer-gnome": ensure => installed }
+  package { "cups": ensure => installed }
+  package { "ghostscript": ensure => installed }
+  package { "unpaper": ensure => installed }
+  package { "printer-driver-all": ensure => installed }
+
+  # PPD-s
+  package { "openprinting-ppds": ensure => installed }
+  package { "hpijs-ppds": ensure => installed }
+  package { "hp-ppd": ensure => installed }
+  package { "foomatic-db-compressed-ppds": ensure => installed }
   
 
 }
