178188603772894642f86b1fa5f557cd626d0e06
tylerecouture <tylerecouture@gmail.com>
2017-04-22 19:47:28 -0700

    autofs manual mount nfshome

--- production/modules/hs_ldap_client/manifests/init.pp
+++ production/modules/hs_ldap_client/manifests/init.pp
@@ -69,7 +69,7 @@ class hs_ldap_client {
   file { 'auto.home':
     path    => '/etc/auto.home',
     ensure  => file,
-    content => "*   tyrell:/home/&",
+    content => "*   tyrell:/nfshome/&",
     require => Package['autofs'],
   }
   file_line { 'auto.master':
