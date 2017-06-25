bd628ebeadc31bea21743da41e1182b7afcc100a
tylerecouture <tylerecouture@gmail.com>
2017-06-25 14:34:20 -0700

    CUPS client.conf

--- production/modules/hs_printers/manifests/init.pp
+++ production/modules/hs_printers/manifests/init.pp
@@ -1,39 +1,15 @@
 class hs_printers {
 
-  #Overwrites color printer for teachers.  
-  #Need to figure out how to add color printer to teacher machine without adding to student machine>
+
+  file { '/etc/cups/client.conf':
+    content  => "ServerName hightower",
+    ensure  => file,
+  }
+
+  # Back up in case CUPS server is unavailable... don't know if this will work or not.
   file { 'printers.conf':
     path    => '/etc/cups/printers.conf',
     source  => "puppet:///modules/hs_printers/printers.conf",
     ensure  => file,
   }
-
-  # https://forge.puppet.com/leoarnold/cups#option-defaults
-  # include '::cups'
-
-  # cups_queue { 'HackerspacePrinter':
-  #   ensure => 'absent',
-  #   model  => 'foomatic-db-compressed-ppds:0/ppd/foomatic-ppd/Lexmark-T644-Postscript.ppd',
-  #   uri    => 'ipp://192.168.43.101', # Replace with your printer's URI
-  #   accepting => true,
-  #   enabled => true,
-  #
-  # }
-
-
-  # https://lauri.võsandi.com/cfgmgmt/puppet-cups.html
-  # Install CUPS and filters
-  # package { "system-config-printer-gnome": ensure => installed }
-  # package { "cups": ensure => installed }
-  # package { "ghostscript": ensure => installed }
-  # package { "unpaper": ensure => installed }
-  # package { "printer-driver-all": ensure => installed }
-  #
-  # # PPD-s
-  # package { "openprinting-ppds": ensure => installed }
-  # package { "hpijs-ppds": ensure => installed }
-  # package { "hp-ppd": ensure => installed }
-  # package { "foomatic-db-compressed-ppds": ensure => installed }
-  
-
 }
