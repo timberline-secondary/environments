1ebc2a84f193ed1c4e30e8b62d31b2638c0ba8e1
tylerecouture <tylerecouture@gmail.com>
2017-09-04 16:17:03 -0700

    remove displaycal

--- production/modules/hs_utilities/manifests/displaycal.pp
+++ production/modules/hs_utilities/manifests/displaycal.pp
@@ -5,32 +5,32 @@ class hs_utilities::displaycal {
 
   # Display Calibration
   package { 'icc-profiles':
-		ensure	=> absent,
+		ensure	=> present,
 	}
   package { 'icc-profiles-free':
-		ensure  => absent,
+		ensure  => present,
 	}
 
   # DisplayCAL deb, https://displaycal.net/#download
-  file { '/opt/displayCAL':
-		ensure	=> directory,
-	}
-	file { 'displayCAL-deb':
-		path	=> '/opt/displayCAL/DisplayCAL.deb',
-		owner	=> root,
-		group	=> root,
-		mode	=> '644',
-		ensure	=> present,
-		source	=> 'puppet:///modules/hs_utilities/displaycal/DisplayCAL.deb',
-		require => File['/opt/displayCAL'],
-	}
+  # file { '/opt/displayCAL':
+		# ensure	=> directory,
+  # }
+  # file { 'displayCAL-deb':
+		# path	=> '/opt/displayCAL/DisplayCAL.deb',
+		# owner	=> root,
+		# group	=> root,
+		# mode	=> '644',
+		# ensure	=> present,
+		# source	=> 'puppet:///modules/hs_utilities/displaycal/DisplayCAL.deb',
+		# require => File['/opt/displayCAL'],
+  # }
 
 	include gdebi
-	package { 'displayCAL':
-		provider => gdebi,
+	package { 'displaycal':
+		#provider => gdebi,
 		ensure	=> absent, # causing pop-up problem!!
-		source => '/opt/displayCAL/DisplayCAL.deb',
-		require => File['displayCAL-deb'],
+		#source => '/opt/displayCAL/DisplayCAL.deb',
+		#require => File['displayCAL-deb'],
 	}
 
 }
