6496cf5dacf61a4a8e369118c1b9c9a9dbb35d4f
tylerecouture <tylerecouture@gmail.com>
2017-12-06 14:59:53 -0800

    latest chrome - id

--- production/modules/hs_student_apps/manifests/chrome.pp
+++ production/modules/hs_student_apps/manifests/chrome.pp
@@ -2,6 +2,7 @@ class hs_student_apps::chrome {
 
   # https://tecadmin.net/install-google-chrome-in-ubuntu/
   # https://ask.puppet.com/question/32773/keeping-the-latest-stable-chrome-installed-ubuntu/
+  # https://www.google.com/linuxrepositories/
 
   include apt
 
@@ -11,7 +12,8 @@ class hs_student_apps::chrome {
     release  => 'stable',
     repos    => 'main',
     key      => {
-      source     => 'https://dl-ssl.google.com/linux/linux_signing_key.pub',
+      id        => '4CCA 1EAF 950C EE4A B839 76DC A040 830F 7FAC 5991',
+      source    => 'https://dl-ssl.google.com/linux/linux_signing_key.pub',
       # 'server' => 'hkp://keyserver.ubuntu.com:80', #  default anyway
     },
   }
