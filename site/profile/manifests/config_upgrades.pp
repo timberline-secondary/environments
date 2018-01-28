6e25215c62a157b1424008fe8a0d014280c239cf
tylerecouture <tylerecouture@gmail.com>
2018-01-28 15:32:28 -0800

    include apt

--- site/profile/manifests/config_upgrades.pp
+++ site/profile/manifests/config_upgrades.pp
@@ -3,6 +3,8 @@ class profile::config_upgrades {
     # https://forge.puppet.com/puppet/unattended_upgrades
     # include unattended_upgrades
 
+    include apt
+
     class { 'unattended_upgrades':
       auto => { 'reboot' => false,
                 'reboot_time' => '4am',
