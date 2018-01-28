5209421014f07fe7bcf2d17309b1b8c78df1f882
tylerecouture <tylerecouture@gmail.com>
2018-01-28 15:35:43 -0800

    include ua

--- site/profile/manifests/config_upgrades.pp
+++ site/profile/manifests/config_upgrades.pp
@@ -4,6 +4,7 @@ class profile::config_upgrades {
     # include unattended_upgrades
 
     include apt
+    include unattended_upgrades
 
     class { 'unattended_upgrades':
       auto => { 'reboot' => false,
