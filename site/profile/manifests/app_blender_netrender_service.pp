59548a2a6f75775ab36b58ecd2480e0a58d67dd0
tylerecouture <tylerecouture@gmail.com>
2018-01-28 19:32:40 -0800

    wget with mode?

--- site/profile/manifests/app_blender_netrender_service.pp
+++ site/profile/manifests/app_blender_netrender_service.pp
@@ -4,7 +4,7 @@ class profile::app_blender_netrender_service {
   include wget
 
   # make executable with mode 755
-  wget::fetch { 'netrender-watchdog.sh':
+  wget::fetch { 'netrender-watchdog.py':
     source  => 'https://raw.githubusercontent.com/timberline-secondary/systemd-blender-netrender/master/netrender-watchdog.py',
     destination => '/user/local/bin/',
     cache_dir   => '/var/cache/wget',
