958535ab119c6e913df8a4429910b3201dc2718c
tylerecouture <tylerecouture@gmail.com>
2018-01-28 19:37:51 -0800

    rawgit?

--- site/profile/manifests/app_blender_netrender_service.pp
+++ site/profile/manifests/app_blender_netrender_service.pp
@@ -5,7 +5,7 @@ class profile::app_blender_netrender_service {
 
   # make executable with mode 755
   wget::fetch { 'netrender-watchdog.py':
-    source  => 'https://raw.githubusercontent.com/timberline-secondary/systemd-blender-netrender/master/netrender-watchdog.py',
+    source  => 'https://cdn.rawgit.com/timberline-secondary/systemd-blender-netrender/master/netrender-watchdog.py',
     destination => '/user/local/bin/',
     cache_dir   => '/var/cache/wget',
     # mode        => '0755',
