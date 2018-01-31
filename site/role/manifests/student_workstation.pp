3783be8202154d3f1b64d81fd84069dc4b0d19f9
tylerecouture <tylerecouture@gmail.com>
2018-01-30 17:38:08 -0800

    flash for student wkstns

--- site/role/manifests/student_workstation.pp
+++ site/role/manifests/student_workstation.pp
@@ -16,7 +16,7 @@ class role::student_workstation {
   include profile::app_audio_suite
 
   include profile::utils_multimedia
-  #include profile::utils_browsers
+  include profile::utils_browsers
   include profile::utils_misc
   include profile::utils_gnome
   include profile::utils_screencast
