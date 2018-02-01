067305dc9e0ee60ff9c53e0c7543313e86c5cdf8
tylerecouture <tylerecouture@gmail.com>
2018-01-31 16:40:58 -0800

    pritner test LexmarkT644

--- site/profile/manifests/config_printers.pp
+++ site/profile/manifests/config_printers.pp
@@ -3,7 +3,7 @@ class profile::config_printers {
   include '::cups'
 
   # Using a suitable model from the output of the command lpinfo -m on the node
-  cups_queue { 'Hackerspace B&W Printer (Lexmark T644)':
+  cups_queue { 'LexmarkT644':
     ensure => 'printer',
     model  => 'foomatic-db-compressed-ppds:0/ppd/foomatic-ppd/Lexmark-T644-Postscript.ppd',
     uri    => 'socket://192.168.43.101', # Replace with your printer's URI
