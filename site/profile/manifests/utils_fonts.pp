45934fe945929db0426f727d6eed667f81427efa
tylerecouture <tylerecouture@gmail.com>
2018-04-09 12:37:26 -0700

    purge msfonts 3.4 cus bug

--- site/profile/manifests/utils_fonts.pp
+++ site/profile/manifests/utils_fonts.pp
@@ -7,12 +7,9 @@ class profile::utils_fonts {
     ensure => latest,
   }
 
-  # package{'ttf-mscorefonts-installer':
-  #   ensure => latest,
-  # }
-
-  package{'msttcorefonts':
-    ensure => latest,
+  # V 3.4 buggy (as of Apr 2018), use deb instead.
+  package{'ttf-mscorefonts-installer':
+    ensure => purged,
   }
 
 
