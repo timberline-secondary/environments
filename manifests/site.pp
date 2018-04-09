74915b892ba89a53e4cd93c44bb21f010bbeedcb
tylerecouture <tylerecouture@gmail.com>
2018-04-09 11:56:27 -0700

    add attendance_workstation

--- manifests/site.pp
+++ manifests/site.pp
@@ -48,8 +48,8 @@ node 'puppet' {
 #   include role::student_workstation
 # }
 
-#1-30
-node /tbl-hackerspace-(0?[1-9]|1[0-9]|2[0-9]|30)-s\.hackerspace\.tbl/ {
+#1-8, 10-30
+node /tbl-hackerspace-(0?[1-8]|1[0-9]|2[0-9]|30)-s\.hackerspace\.tbl/ {
   include role::student_workstation
 }
 
