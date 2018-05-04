c950eec938b9c10db220ab997530cdf9c2738a47
tylerecouture <tylerecouture@gmail.com>
2018-05-04 11:00:13 -0700

    update discord

--- site/profile/manifests/utils_discord.pp
+++ site/profile/manifests/utils_discord.pp
@@ -2,15 +2,15 @@ class profile::utils_discord {
   include wget
   include gdebi
 
-  wget::fetch { 'discord-0.0.4.deb':
-    source      => 'https://dl.discordapp.net/apps/linux/0.0.4/discord-0.0.4.deb',
+  wget::fetch { 'discord-0.0.5.deb':
+    source      => 'https://dl.discordapp.net/apps/linux/0.0.4/discord-0.0.5.deb',
     destination => '/tmp/',
     cache_dir   => '/var/cache/wget',
   } ~>
     package { 'discord':
       provider => gdebi,
       ensure   => latest,
-      source   => '/tmp/discord-0.0.4.deb',
+      source   => '/tmp/discord-0.0.5.deb',
     }
 
 }
