6f33e03dab93786c1bba1f8f42ad41290ee1675a
tylerecouture <tylerecouture@gmail.com>
2018-05-14 13:50:36 -0700

    playonlinux key sig test 4

--- site/profile/manifests/utils_play_on_linux.pp
+++ site/profile/manifests/utils_play_on_linux.pp
@@ -8,7 +8,8 @@ class profile::utils_play_on_linux {
     repos    => 'main',
     key      => {
       'id'     => 'D8538615FEAC389FDF848C2273F0D6E88E3D6C3A',
-      'server' => 'https://keyserver.ubuntu.com/',
+      #'server' => 'https://keyserver.ubuntu.com',
+      'source'  => 'http://deb.playonlinux.com/public.gpg',
     },
   }
 
