a097d26a6e837c4e81388f00a7d8f5ed1b46b50d
tylerecouture <tylerecouture@gmail.com>
2018-11-19 16:18:12 -0800

    test godot desktopfile

--- site/profile/manifests/dev_godot.pp
+++ site/profile/manifests/dev_godot.pp
@@ -5,41 +5,32 @@ class profile::dev_godot {
     download_base_url => 'https://downloads.tuxfamily.org/godotengine',
     distribution_name => "3.0.6/Godot_v3.0.6-stable_x11.64.zip",
     dest_folder       => '/opt',
-    creates           => '/opt/Godot',
+    creates           => '/opt/Godot_v3.0.6-stable_x11.64',
     uncompress        => 'zip',
     user              => root,
     group             => root,
   }
 
 
-  # download_uncompress { 'godot-binary':
-  #   distribution_name => 'https://downloads.tuxfamily.org/godotengine/3.0.6/Godot_v3.0.6-stable_x11.64.zip',
-  #   dest_folder       => '/opt',
-  #   creates           => '/opt/Godot',
-  #   uncompress        => 'zip',
-  #   user              => root,
-  #   group             => root,
-  # }
-#
-#   file { 'godot.desktop':
-#     ensure  => file,
-#     owner   => root,
-#     group   => root,
-#     mode    => '755',
-#     content => "[Desktop Entry]\n
-# Encoding=UTF-8\n
-# Version=3.0.6\n
-# Name=Godot\n
-# Comment=Create different games\n
-# Exec=/opt/Godot/Godot\n
-# Icon=godot\n
-# Terminal=false\n
-# Type=Application\n
-# Categories=Development; Games;\n
-# Name[en_US]=godot.desktop\n
-# GenericName[en_US]=Game development platform\n
-# Comment[en_US]=Create different games!\n",
-#   }
-#
+  file { 'godot.desktop':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '755',
+    content => "[Desktop Entry]\n
+Encoding=UTF-8\n
+Version=3.0.6\n
+Name=Godot\n
+Comment=Create different games\n
+Exec=/opt/Godot_v3.0.6-stable_x11.64\n
+Icon=godot\n
+Terminal=false\n
+Type=Application\n
+Categories=Development; Games;\n
+Name[en_US]=godot.desktop\n
+GenericName[en_US]=Game development platform\n
+Comment[en_US]=Create different games!\n",
+  }
+
 
 }
