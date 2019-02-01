ca0c02f6bc3143ff4e5a3d1890214530e9520937
tylerecouture <tylerecouture@gmail.com>
2019-02-01 09:27:41 -0800

    discord 0.0.8

--- site/profile/manifests/utils_discord.pp
+++ site/profile/manifests/utils_discord.pp
@@ -2,15 +2,15 @@ class profile::utils_discord {
   include wget
   include gdebi
 
-  wget::fetch { 'discord-latest':
-    source      => 'https://discordapp.com/api/download?platform=linux&format=deb',
+  wget::fetch { 'discord-0.0.8.deb':
+    source      => 'https://dl.discordapp.net/apps/linux/0.0.8/discord-0.0.8.deb',
     destination => '/tmp/',
     cache_dir   => '/var/cache/wget',
   } ~>
     package { 'discord':
       provider => gdebi,
       ensure   => latest,
-      source   => '/tmp/discord-latest',
+      source   => '/tmp/discord-0.0.8.deb',
     }
 
 }
