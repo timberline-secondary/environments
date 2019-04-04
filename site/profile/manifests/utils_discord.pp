8e9f19acc169fea6bdf1c420e2b66c225ff9a5aa
tylerecouture <tylerecouture@gmail.com>
2019-04-04 09:53:56 -0700

    discord 0.9 update

--- site/profile/manifests/utils_discord.pp
+++ site/profile/manifests/utils_discord.pp
@@ -2,15 +2,15 @@ class profile::utils_discord {
   include wget
   include gdebi
 
-  wget::fetch { 'discord-0.0.8.deb':
-    source      => 'https://dl.discordapp.net/apps/linux/0.0.8/discord-0.0.8.deb',
+  wget::fetch { 'discord-0.0.9.deb':
+    source      => 'https://dl.discordapp.net/apps/linux/0.0.9/discord-0.0.9.deb',
     destination => '/tmp/',
     cache_dir   => '/var/cache/wget',
   } ~>
     package { 'discord':
       provider => gdebi,
       ensure   => latest,
-      source   => '/tmp/discord-0.0.8.deb',
+      source   => '/tmp/discord-0.0.9.deb',
     }
 
 }
