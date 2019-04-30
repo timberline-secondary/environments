3728d88658a1e458d0a966a09f045644b2666abb
tylerecouture <tylerecouture@gmail.com>
2019-04-30 09:47:46 -0700

    update kaku

--- site/profile/manifests/utils_multimedia.pp
+++ site/profile/manifests/utils_multimedia.pp
@@ -29,15 +29,15 @@ class profile::utils_multimedia {
   include wget
   include gdebi
 
-  wget::fetch { 'Kaku-1.9.0-amd64.deb':
-    source  => 'https://github.com/EragonJ/Kaku/releases/download/1.9.0/Kaku-1.9.0-amd64.deb',
+  wget::fetch { 'Kaku-2.0.2-amd64.deb':
+    source  => 'https://github.com/EragonJ/Kaku/releases/download/2.0.2/Kaku_2.0.2_amd64.deb',
     destination => '/tmp/',
     cache_dir   => '/var/cache/wget',
   } ~>
   package { 'kaku':
     provider => gdebi,
     ensure	=> latest,
-    source => '/tmp/Kaku-1.9.0-amd64.deb',
+    source => '/tmp/Kaku-2.0.2-amd64.deb',
   }
 
   # ###########
