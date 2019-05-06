5df8616a13fd94f20168b0c63b7a17f7f1088d96
tylerecouture <tylerecouture@gmail.com>
2019-05-06 10:05:19 -0700

    try deb with keys for vs_code

--- site/profile/manifests/dev_vs_code.pp
+++ site/profile/manifests/dev_vs_code.pp
@@ -6,18 +6,36 @@ class profile::dev_vs_code {
   #   # install_options => '--classic'  # this doesn't work I think.
   # }
 
-  include wget
-  include gdebi
+  # include wget
+  # include gdebi
+  #
+  # wget::fetch { 'code_latest_amd64.deb':
+  #   source      => 'https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable',
+  #   destination => '/tmp/',
+  #   cache_dir   => '/var/cache/wget',
+  # } ~>
+  #   package { 'vs_code':
+  #     provider => gdebi,
+  #     ensure   => latest,
+  #     source   => '/tmp/code_latest_amd64.deb',
+  #   }
 
-  wget::fetch { 'code_latest_amd64.deb':
-    source      => 'https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable',
-    destination => '/tmp/',
-    cache_dir   => '/var/cache/wget',
-  } ~>
-    package { 'vs_code':
-      provider => gdebi,
-      ensure   => latest,
-      source   => '/tmp/code_latest_amd64.deb',
-    }
+  include apt
+
+  apt::source { 'vs-code-repo':
+    comment  => 'https://code.visualstudio.com/docs/setup/linux',
+    location => 'https://packages.microsoft.com/repos/vscode',
+    release  => 'stable',
+    repos    => 'main',
+    key      => {
+      id        => 'BC528686B50D79E339D3721CEB3E94ADBE1229CF',
+      source    => 'https://packages.microsoft.com/keys/microsoft.asc',
+    },
+  }
+
+  package { 'code':
+    ensure  => latest,
+    require => [ Class['apt::update'], Apt::Source['vs-code-repo'] ],
+  }
 
 }
