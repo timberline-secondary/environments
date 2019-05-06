3340133c91c1a58ae2e33a9d9db3a615bd4a3e0a
tylerecouture <tylerecouture@gmail.com>
2019-05-06 09:32:53 -0700

    try vscode deb

--- site/profile/manifests/dev_vs_code.pp
+++ site/profile/manifests/dev_vs_code.pp
@@ -1,8 +1,23 @@
 class profile::dev_vs_code {
 
-  package { 'code':
-    ensure   => latest,
-    provider => snap,
-    install_options => '--classic'
-  }
+  # package { 'code':
+  #   ensure   => latest,
+  #   provider => snap,
+  #   install_options => '--classic'  # this doesn't work I think.
+  # }
+
+  include wget
+  include gdebi
+
+  wget::fetch { 'vs_code':
+    source      => 'https://go.microsoft.com/fwlink/?LinkID=760868',
+    destination => '/tmp/',
+    cache_dir   => '/var/cache/wget',
+  } ~>
+    package { 'vs_code':
+      provider => gdebi,
+      ensure   => latest,
+      source   => '/tmp/vs_code',
+    }
+
 }
