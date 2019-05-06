922892af09c3e337596b1ea56deddc5a4d8c8bf8
tylerecouture <tylerecouture@gmail.com>
2019-05-06 16:33:22 -0700

    push vs_code to lab

--- site/profile/manifests/dev/vs_code.pp
+++ site/profile/manifests/dev/vs_code.pp
@@ -1,45 +1,7 @@
 class profile::dev::vs_code {
-
+  # https://code.visualstudio.com/docs/setup/linux
 
   include wget
-  include gdebi
-
-  # deb direct install
-  # wget::fetch { 'code_1.33.1_amd64.deb':
-  #   source      => 'https://packages.microsoft.com/repos/vscode/pool/main/c/code/code_1.33.1-1554971066_amd64.deb',
-  #   destination => '/tmp/',
-  #   cache_dir   => '/var/cache/wget',
-  # } ~>
-  #   package { 'code':
-  #     provider => gdebi,
-  #     ensure   => latest,
-  #     source   => '/tmp/code_1.33.1_amd64.deb',
-  #   }
-
-  # SNAP install
-
-  # package { 'code':
-  #   ensure   => purged,
-  #   provider => snap,
-  #   # install_options => '--classic'  # this doesn't work I think.
-  # }
-
-  # include wget
-  # include gdebi
-  #
-  # wget::fetch { 'code_latest_amd64.deb':
-  #   source      => 'https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable',
-  #   destination => '/tmp/',
-  #   cache_dir   => '/var/cache/wget',
-  # } ~>
-  #   package { 'vs_code':
-  #     provider => gdebi,
-  #     ensure   => latest,
-  #     source   => '/tmp/code_latest_amd64.deb',
-  #   }
-
-
-  # apt install
 
   # use this to install microsoft keys
   wget::fetch { 'packages-microsoft-prod.deb':
