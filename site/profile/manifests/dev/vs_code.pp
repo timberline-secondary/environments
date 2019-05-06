9e06dbe77bb0e5d32d0cebac0a357f1b94e76c8d
tylerecouture <tylerecouture@gmail.com>
2019-05-06 16:20:14 -0700

    more fiddle vs_code

---
+++ site/profile/manifests/dev/vs_code.pp
@@ -0,0 +1,76 @@
+class profile::dev_vs_code {
+
+
+  include wget
+  include gdebi
+
+  # deb direct install
+  # wget::fetch { 'code_1.33.1_amd64.deb':
+  #   source      => 'https://packages.microsoft.com/repos/vscode/pool/main/c/code/code_1.33.1-1554971066_amd64.deb',
+  #   destination => '/tmp/',
+  #   cache_dir   => '/var/cache/wget',
+  # } ~>
+  #   package { 'code':
+  #     provider => gdebi,
+  #     ensure   => latest,
+  #     source   => '/tmp/code_1.33.1_amd64.deb',
+  #   }
+
+  # SNAP install
+
+  # package { 'code':
+  #   ensure   => purged,
+  #   provider => snap,
+  #   # install_options => '--classic'  # this doesn't work I think.
+  # }
+
+  # include wget
+  # include gdebi
+  #
+  # wget::fetch { 'code_latest_amd64.deb':
+  #   source      => 'https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable',
+  #   destination => '/tmp/',
+  #   cache_dir   => '/var/cache/wget',
+  # } ~>
+  #   package { 'vs_code':
+  #     provider => gdebi,
+  #     ensure   => latest,
+  #     source   => '/tmp/code_latest_amd64.deb',
+  #   }
+
+
+  # apt install
+
+  # use this to install microsoft keys
+  wget::fetch { 'packages-microsoft-prod.deb':
+    source  => 'https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb',
+    destination => '/tmp/',
+    cache_dir   => '/var/cache/wget',
+  } ~>
+  package { 'Microsoft-product-packages':
+    provider => gdebi,
+    ensure	=> latest,
+    source => '/tmp/packages-microsoft-prod.deb',
+  }
+
+  apt::source { 'microsoft-vscode':
+    comment  => 'https://code.visualstudio.com/docs/setup/linux',
+    location => 'https://packages.microsoft.com/repos/vscode',
+    release  => 'stable',
+    repos    => 'main',
+    # key      => {
+    #   id        => '',
+    #   source    => '',
+    # },
+  }
+
+  package { 'apt-transport-https':
+    ensure  => latest,
+  }
+
+  package { 'code':
+    ensure  => latest,
+    require => [ Class['apt::update'], Package['apt-transport-https'], Apt::Source['microsoft-vscode'] ],
+  }
+
+}
--- site/role/manifests/student_workstation.pp
+++ site/role/manifests/student_workstation.pp
@@ -38,6 +38,6 @@ class role::student_workstation {
   include profile::dev_unity3d
   include profile::dev_git_desktop
   include profile::dev_godot
-  include profile::dev_vs_code
+  #include profile::dev_vs_code
 
 }
