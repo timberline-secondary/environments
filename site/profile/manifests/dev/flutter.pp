f6c639fa29f842848087dc2bb377743a781a7565
tylerecouture <tylerecouture@gmail.com>
2019-05-10 13:38:06 -0700

    android-sdk test

--- site/profile/manifests/dev/flutter.pp
+++ site/profile/manifests/dev/flutter.pp
@@ -1,9 +1,15 @@
 class profile::dev::flutter {
 
+  package { 'android-sdk':
+    ensure => latest,
+  }
+
   apt::ppa { 'ppa:maarten-fonville/android-studio':
     ensure => present,
   }
   package { 'android-studio':
+    # places Android studio in : /opt/android-studio
+    # there is no desktop icon/start menu link
     ensure  => latest,
     require => [ Class['apt::update'], Apt::Ppa['ppa:maarten-fonville/android-studio'] ],
   }
