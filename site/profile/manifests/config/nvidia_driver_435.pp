6ff5c039e8ef4655ae1abb6b1e90fd827e303409
tylerecouture <tylerecouture@gmail.com>
2019-10-11 13:41:37 -0700

    1804 driver fiddling...

---
+++ site/profile/manifests/config/nvidia_driver_435.pp
@@ -0,0 +1,21 @@
+# requires rgb-nvidia module
+class profile::config::nvidia_driver_430 {
+
+  include apt
+
+  apt::ppa { 'ppa:graphics-drivers/ppa':
+    ensure => present,
+  }
+
+  package {'nvidia-430 ':
+    ensure  => latest,
+    require => [ Class['apt::update'], Apt::Ppa['ppa:graphics-drivers/ppa'] ],
+    notify  => Reboot['after_nvidia'],
+  }
+
+  # https://forge.puppet.com/puppetlabs/reboot
+  reboot { 'after_nvidia':
+    apply  => finished,
+  }
+
+}
