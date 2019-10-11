6a137f4c2126dbf2d6fa6858776f28afd2665661
tylerecouture <tylerecouture@gmail.com>
2019-10-11 16:23:50 -0700

    blender 2.79

---
+++ site/profile/manifests/apps/blender_2_79.pp
@@ -0,0 +1,17 @@
+class profile::app::blender_2_79 {
+
+  # include apt
+  
+  # apt::ppa { 'ppa:thomas-schiex/blender':
+  #   ensure => absent,
+  # }
+
+  # package {'libopensubdiv':
+  #   require => Package['blender'],
+  # }
+
+  package { 'blender':
+    ensure   => "2.79.b+dfsg0-1",
+    #require => [ Class['apt::update'], Apt::Ppa['ppa:thomas-schiex/blender'] ],
+  }
+  
