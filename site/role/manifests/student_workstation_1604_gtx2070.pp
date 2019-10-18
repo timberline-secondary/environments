7fd92eba7af0c0498999831ba58e2b6d657a42c3
tylerecouture <tylerecouture@gmail.com>
2019-10-18 13:42:02 -0700

    add all profiles to 1604_gtx2070

--- site/role/manifests/student_workstation_1604_gtx2070.pp
+++ site/role/manifests/student_workstation_1604_gtx2070.pp
@@ -9,39 +9,38 @@ class role::student_workstation_1604_gtx2070 {
   include profile::config_ldap_client
   include profile::config_upgrades
   include profile::config_restart_daily
-  # include profile::config_printers
+  include profile::config_printers
 
   include profile::config::nvidia_driver_430
 
-  # include profile::app_blender
-  # #include profile::app_blender_netrender_service
-  # include profile::app_krita
-  # include profile::app_gimp
-  # include profile::app_cura
-  # include profile::app_audio_suite
-  # include profile::app_games
-  # include profile::app_darktable
-  # include profile::app_steam
+  include profile::app_blender
+  #include profile::app_blender_netrender_service
+  include profile::app_krita
+  include profile::app_gimp
+  include profile::app_cura
+  include profile::app_audio_suite
+  include profile::app_games
+  include profile::app_darktable
+  include profile::app_steam
 
-  # include profile::utils_multimedia
-  # include profile::utils_browsers
-  # include profile::utils_misc
-  # include profile::utils_gnome
-  # include profile::utils_screencast
-  # include profile::utils_desktops
-  # include profile::utils_discord
-  # include profile::utils_fonts
-  # include profile::utils_play_on_linux
+  include profile::utils_multimedia
+  include profile::utils_browsers
+  include profile::utils_misc
+  include profile::utils_gnome
+  include profile::utils_screencast
+  include profile::utils_desktops
+  include profile::utils_discord
+  include profile::utils_fonts
+  include profile::utils_play_on_linux
 
-  # include profile::admin_ssh
+  include profile::admin_ssh
 
-  # include profile::dev_java
-  # include profile::dev_python
-  # include profile::dev_general_utils
-  # # include profile::dev_unity3d
-  # include profile::dev_git_desktop
-  # include profile::dev_godot
-  # include profile::dev::vs_code
+  include profile::dev_java
+  include profile::dev_python
+  include profile::dev_general_utils
+  include profile::dev_git_desktop
+  include profile::dev_godot
+  include profile::dev::vs_code
 
 }
 
