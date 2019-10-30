6ce7ec1197f79d7ff350d66b958bccb71680c263
tylerecouture <tylerecouture@gmail.com>
2019-10-30 15:09:36 -0700

    new cpu hostnames with h10 AND s

--- manifests/site.pp
+++ manifests/site.pp
@@ -77,7 +77,7 @@ node /tbl-h10-test1804-(0?[1-9]|1[0-9]|2[0-9]|3[0-9])\.hackerspace\.tbl/ {
   include role::student_workstation_1804
 }
 
-node /tbl-h10-(0?[1-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9])\.hackerspace\.tbl/ {
+node /tbl-h10-(0?[1-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9])-s\.hackerspace\.tbl/ {
   include role::student_workstation_1604_gtx2070
 }
 
