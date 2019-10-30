f17def59e584c52a9c70550fdc7a3f6f420f2e9a
tylerecouture <tylerecouture@gmail.com>
2019-10-30 14:32:25 -0700

    make cura executable

--- site/profile/manifests/apps/cura.pp
+++ site/profile/manifests/apps/cura.pp
@@ -2,7 +2,7 @@
 class profile::apps::cura {
 
   $cura_version = '4.3.0'
-  $cura_executable = "/opt/Cura-${cura_version}"
+  $cura_executable = "/opt/Cura-${cura_version}.AppImage"
   $cura_url = "https://github.com/Ultimaker/Cura/releases/download/4.3.0/Ultimaker_Cura-${cura_version}.AppImage"
   $cura_icon = "/usr/share/icons/cura.png"
   $cura_desktop_file = '/usr/share/applications/cura.desktop'
@@ -10,6 +10,7 @@ class profile::apps::cura {
   archive { $cura_executable:
     ensure => present,
     source => $cura_url,
+    mode   => '0755'
   }
 
   archive { $cura_icon:
@@ -22,7 +23,7 @@ class profile::apps::cura {
     ensure  => file,
     owner   => root,
     group   => root,
-    mode    => '755',
+    mode    => '0644',
     content => "[Desktop Entry] 
 Encoding=UTF-8
 Version=${cura_version}
@@ -32,10 +33,7 @@ Exec=${cura_executable}
 Icon=${cura_icon}
 Terminal=false
 Type=Application
-Categories=Utility; Printing;
-Name[en_US]=cura.desktop
-GenericName[en_US]=3D printer / slicing GUI built on top of the Uranium framework 
-Comment[en_US]=3D print stuff",
+Categories=Utility; Printing; ",
   }
 
 
