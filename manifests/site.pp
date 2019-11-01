5be6c29c7076ae642144c3e9f22e8e7bf3d6b98a
tylerecouture <tylerecouture@gmail.com>
2019-11-01 12:33:39 -0700

    g122 namign convention

--- manifests/site.pp
+++ manifests/site.pp
@@ -77,7 +77,13 @@ node /tbl-h10-test1804-(0?[1-9]|1[0-9]|2[0-9]|3[0-9])\.hackerspace\.tbl/ {
   include role::student_workstation_1804
 }
 
+# tbl-h10-#-s
 node /tbl-h10-(0?[1-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9])-s\.hackerspace\.tbl/ {
+  include role::student_workstation_1604_gtx2070
+}
+
+# tbl-g122-#-s
+node /tbl-g122-(0?[1-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9])-s\.hackerspace\.tbl/ {
   include role::student_workstation_1604_gtx2070
 }
 
