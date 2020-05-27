7b7abdbf6ff17281fa7448a824c88ab6321a2a87
tylerecouture <tylerecouture@gmail.com>
2020-05-27 13:35:28 -0700

    add nvidia driver to base

--- site/profile/manifests/desktop/base.pp
+++ site/profile/manifests/desktop/base.pp
@@ -5,8 +5,14 @@ class profile::desktop::base {
     ensure  => latest,
   }
 
-  package { 'ubuntu-drivers':
+  package { 'nvidia-drivers-440':
     ensure  => latest,
+    require => Package['ubuntu-desktop'],
+    notify  => Reboot['after_run'],
+  }
+
+  reboot { 'after_run':
+    apply   => finished,
   }
 
 }
