fbbf103148238c5e5b4b0b86f329c540143b404b
tylerecouture <tylerecouture@gmail.com>
2020-06-02 12:42:11 -0700

    allow-guest=true

--- site/profile/manifests/config/lightdm.pp
+++ site/profile/manifests/config/lightdm.pp
@@ -14,36 +14,36 @@ class profile::config::lightdm {
   file {'default-display-manager':
     ensure  => file,
     path    => '/etc/X11/default-display-manager',
-    content => '/usr/sbin/lightdm',
+    content => "/usr/sbin/lightdm\n",
     require => Package['lightdm'],
     notify  => Reboot['after_run']
   }
-  # debconf { 'default-x-display-manager':
-  #   package => 'lightdm',
-  #   item    => 'shared/default-x-display-manager',
-  #   type    => 'select',
-  #   value   => 'lightdm',
-  #   seen    => true,
-  #   require => Package['lightdm'],
-  #   notify  => Exec['dpkg-reconfigure lightdm']
-  # }
+  debconf { 'default-x-display-manager':
+    package => 'lightdm',
+    item    => 'shared/default-x-display-manager',
+    type    => 'select',
+    value   => 'lightdm',
+    seen    => true,
+    require => Package['lightdm'],
+    notify  => Exec['dpkg-reconfigure lightdm']
+  }
 
   file { 'lightdm.conf':
     ensure  => file,
     path    => '/etc/lightdm/lightdm.conf',
-    content => '# MANAGED BY PUPPETMASTER
+    content => "# MANAGED BY PUPPETMASTER
 [SeatDefaults]
+allow-guest=true
 greeter-show-manual-login=true
 greeter-hide-users=true
-greeter-setup-script=/usr/bin/numlockx on
-',
+greeter-setup-script=/usr/bin/numlockx on\n",
     require => Package['lightdm'],
   }
 
-  # exec {'dpkg-reconfigure lightdm':
-  #   path        => ['/usr/bin/', '/usr/sbin'],
-  #   refreshonly => true,
-  #   notify      => Reboot['after_run']
-  # }
+  exec {'dpkg-reconfigure lightdm':
+    path        => ['/usr/bin/', '/usr/sbin'],
+    refreshonly => true,
+    notify      => Reboot['after_run']
+  }
 
 }
