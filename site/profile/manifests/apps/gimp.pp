fc7cb9a5fcaf50d3ddc8902121cc74526321637f
tylerecouture <tylerecouture@gmail.com>
2020-06-02 13:39:59 -0700

    gimp and dark theme default

---
+++ site/profile/manifests/apps/gimp.pp
@@ -0,0 +1,22 @@
+#
+class profile::app::gimp {
+
+  package { 'gimp':
+      ensure  => latest,
+    #   require => [ Class['apt::update'], Apt::Ppa['ppa:otto-kesselgulasch/gimp'] ],
+  }
+#   package { 'gmic':
+#       ensure  => absent,
+#       require => [ Class['apt::update'], Package['gimp'] ],
+#   }
+#   package { 'gimp-gmic':
+#       ensure  => absent,
+#       require => [ Class['apt::update'], Package['gimp'],  Package['gmic'] ],
+#   }
+
+#   package { 'gimp-plugin-registry':
+#       ensure  => absent,
+#       require => [ Class['apt::update'], Package['gimp'] ],
+#     }
+
+}
