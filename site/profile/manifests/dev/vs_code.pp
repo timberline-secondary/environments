85b7c3ae6d30abda6a81e17df9e134dc8b973f1e
tylerecouture <tylerecouture@gmail.com>
2020-06-02 13:55:59 -0700

    20.04 WIP

--- site/profile/manifests/dev/vs_code.pp
+++ site/profile/manifests/dev/vs_code.pp
@@ -1,38 +1,47 @@
+#
 class profile::dev::vs_code {
-  # https://code.visualstudio.com/docs/setup/linux
 
-  include wget
-
-  # use this to install microsoft keys
-  wget::fetch { 'packages-microsoft-prod.deb':
-    source  => 'https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb',
-    destination => '/tmp/',
-    cache_dir   => '/var/cache/wget',
-  } ~>
-  package { 'Microsoft-product-packages':
-    provider => gdebi,
-    ensure	=> latest,
-    source => '/tmp/packages-microsoft-prod.deb',
-  }
-
-  apt::source { 'microsoft-vscode':
-    comment  => 'https://code.visualstudio.com/docs/setup/linux',
-    location => 'https://packages.microsoft.com/repos/vscode',
-    release  => 'stable',
-    repos    => 'main',
-    # key      => {
-    #   id        => '',
-    #   source    => '',
-    # },
-  }
-
-  package { 'apt-transport-https':
-    ensure  => latest,
-  }
+  include ::snapd
 
   package { 'code':
-    ensure  => latest,
-    require => [ Class['apt::update'], Package['apt-transport-https'], Apt::Source['microsoft-vscode'] ],
+    ensure   => latest,
+    provider => snap,
+    # require => [ Class['apt::update'], Package['apt-transport-https'], Apt::Source['microsoft-vscode'] ],
   }
+  # # https://code.visualstudio.com/docs/setup/linux
+
+  # include wget
+
+  # # use this to install microsoft keys
+  # wget::fetch { 'packages-microsoft-prod.deb':
+  #   source  => 'https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb',
+  #   destination => '/tmp/',
+  #   cache_dir   => '/var/cache/wget',
+  # } ~>
+  # package { 'Microsoft-product-packages':
+  #   provider => gdebi,
+  #   ensure	=> latest,
+  #   source => '/tmp/packages-microsoft-prod.deb',
+  # }
+
+  # apt::source { 'microsoft-vscode':
+  #   comment  => 'https://code.visualstudio.com/docs/setup/linux',
+  #   location => 'https://packages.microsoft.com/repos/vscode',
+  #   release  => 'stable',
+  #   repos    => 'main',
+  #   # key      => {
+  #   #   id        => '',
+  #   #   source    => '',
+  #   # },
+  # }
+
+  # package { 'apt-transport-https':
+  #   ensure  => latest,
+  # }
+
+  # package { 'code':
+  #   ensure  => latest,
+  #   require => [ Class['apt::update'], Package['apt-transport-https'], Apt::Source['microsoft-vscode'] ],
+  # }
 
 }
--- site/role/manifests/student_workstation_2004_rtx2070.pp
+++ site/role/manifests/student_workstation_2004_rtx2070.pp
@@ -30,8 +30,6 @@ class role::student_workstation_2004_rtx2070 {
 
   # include profile::utils_multimedia
   # include profile::utils_browsers
-  # include profile::utils_misc
-  # include profile::utils_gnome
   # include profile::utils_screencast
   # include profile::utils_desktops
   # include profile::utils_discord
@@ -42,8 +40,7 @@ class role::student_workstation_2004_rtx2070 {
   # include profile::dev_python
   # include profile::dev_general_utils
   # include profile::dev_git_desktop
-  # include profile::dev_godot
-  # include profile::dev::vs_code
+  include profile::dev::vs_code
   # include profile::dev::processing
   # include profile::dev::virtual_machines
 
