bb1f5e1c7fdaa41fc94e605a8b049e7d2000a9e8
tylerecouture <tylerecouture@gmail.com>
2020-06-03 17:11:54 +0000

    python dev packages

--- site/profile/manifests/dev/python.pp
+++ site/profile/manifests/dev/python.pp
@@ -1,12 +1,17 @@
 #
 class profile::dev::python {
 
-  package { 'python-is-python3':
-      ensure => latest,
-  }
+  package { 'python-is-python3':ensure => latest }
+  package { 'idle3': ensure  => latest }
+  package { 'python3-pip': ensure  => latest }
+  package { 'python3-venv': ensure  => latest }
 
-  package { 'idle3':
-    ensure  => latest,
-  }
+  # Python 3 Development packages
+  # https://www.digitalocean.com/community/tutorials/how-to-install-python-3-and-set-up-a-programming-environment-on-an-ubuntu-20-04-server
+
+  package { 'build-essential': ensure  => latest }
+  package { 'libssl-dev': ensure  => latest }
+  package { 'libffi-dev': ensure  => latest }
+  package { 'python3-dev': nsure  => latest }
 
 }
