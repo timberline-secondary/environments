5911405dd8e40a6553a398c594693d00812e253c
tylerecouture <tylerecouture@gmail.com>
2020-06-08 13:08:30 -0700

    codecs

--- site/profile/manifests/multimedia/players_and_viewers.pp
+++ site/profile/manifests/multimedia/players_and_viewers.pp
@@ -1,7 +1,11 @@
 #
 class profile::multimedia::players_and_viewers {
-  
+
   package {'vlc':
+    ensure => latest,
+  }
+
+  package {'vlc-plugin-fluidsynth':
     ensure => latest,
   }
 
--- site/profile/manifests/multimedia/utils_and_codecs.pp
+++ site/profile/manifests/multimedia/utils_and_codecs.pp
@@ -1,6 +1,11 @@
 #
 class profile::multimedia::utils_and_codecs {
+  
   package {'ffmpeg':
+    ensure => latest,
+  }
+
+  package {'ubuntu-restricted-extras':
     ensure => latest,
   }
 
