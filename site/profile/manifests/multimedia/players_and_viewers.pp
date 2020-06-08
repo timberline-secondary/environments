7319124d8c107151547ed841491657c508a68af2
tylerecouture <tylerecouture@gmail.com>
2020-06-08 14:34:12 -0700

    kaku

--- site/profile/manifests/multimedia/players_and_viewers.pp
+++ site/profile/manifests/multimedia/players_and_viewers.pp
@@ -13,4 +13,27 @@ class profile::multimedia::players_and_viewers {
     ensure => latest,
   }
 
+
+    #############
+  #
+  # Kaku
+  # http://kaku.rocks/
+  #
+  #############
+
+  include gdebi
+
+  $kaku_version = '2.0.2'
+
+  archive{ '/tmp/Kaku.deb':
+    ensure  => latest
+    source  => "https://github.com/EragonJ/Kaku/releases/download/${kaku_version}/Kaku_${kaku_version}_amd64.deb",
+  }
+  package { 'kaku':
+    provider => gdebi,
+    ensure   => latest,
+    source   => '/tmp/Kaku.deb',
+    subscribe=> Archive['/tmp/Kaku.deb']
+  }
+
 }
