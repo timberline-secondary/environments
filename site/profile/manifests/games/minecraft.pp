35b677e9f23f52758f512459646554316016b66f
tylerecouture <tylerecouture@gmail.com>
2020-06-08 12:30:30 -0700

    minefcraft:

--- site/profile/manifests/games/minecraft.pp
+++ site/profile/manifests/games/minecraft.pp
@@ -1,7 +1,17 @@
 class profile::games::minecraft {
 
-  package {'minecraft':
-    ensure => latest,
+  include gdebi
+
+  archive { '/tmp/minecraft.deb':
+    ensure => present,
+    source      => 'https://launcher.mojang.com/download/Minecraft.deb',
+  }
+
+  package { 'minecraft':
+      provider => gdebi,
+      ensure   => latest,
+      source   => '/tmp/minecraft.deb',
+      subscribe=> Archive['/tmp/minecraft.deb']
   }
 
 }
