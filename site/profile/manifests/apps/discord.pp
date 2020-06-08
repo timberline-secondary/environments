1c3ef70c7cbd51e1953da2d8d84f2356b944c70e
tylerecouture <tylerecouture@gmail.com>
2020-06-08 12:20:13 -0700

    discord dl with archive

--- site/profile/manifests/apps/discord.pp
+++ site/profile/manifests/apps/discord.pp
@@ -4,15 +4,18 @@ class profile::apps::discord {
 
   # $version = '0.0.9'
 
-  wget::fetch { 'discord.deb':
+  archive { '/tmp/discord.deb':
+    ensure => present,
     source      => 'https://discordapp.com/api/download?platform=linux&format=deb',
-    destination => '/tmp/',
-    cache_dir   => '/var/cache/wget',
-  } ~>
-    package { 'discord':
+    # destination => '/tmp/',
+    # cache_dir   => '/var/cache/wget',
+  }
+
+  package { 'discord':
       provider => gdebi,
       ensure   => latest,
       source   => '/tmp/discord.deb',
-    }
+      subscribe=> Archive['/tmp/discord.deb']
+  }
 
 }
