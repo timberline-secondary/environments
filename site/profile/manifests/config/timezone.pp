9841d1c5bff2450b53f348e4d96de832c7c92751
tylerecouture <tylerecouture@gmail.com>
2020-06-09 02:27:28 +1000

    Vancouver timezone

--- site/profile/manifests/config/timezone.pp
+++ site/profile/manifests/config/timezone.pp
@@ -2,7 +2,7 @@
 class profile::config::timezone {
 
   exec {'set-timezone':
-    command => "timedatectl set-timezone Australia/Sydney",
+    command => "timedatectl set-timezone America/Vancouver",
     path    => "/usr/bin/",
   }
 
