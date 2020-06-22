f12797f6554beb5be37bcf63b835151bc95965fd
tylerecouture <tylerecouture@gmail.com>
2020-06-22 15:15:02 -0700

    clean up steam string

--- site/profile/manifests/games/steam.pp
+++ site/profile/manifests/games/steam.pp
@@ -24,7 +24,7 @@ class profile::games::steam {
   file_line { 'steamapps':
     path      => '/usr/bin/steam',
     match     => '^cd "\$LAUNCHSTEAMDIR"\n',
-    line      => "cd \"$LAUNCHSTEAMDIR\"
+    line      => 'cd "$LAUNCHSTEAMDIR"
 
 #############################################
 #
@@ -32,56 +32,13 @@ class profile::games::steam {
 #
 #############################################
 
-rm -r \"/home/$USER/.local/share/Steam/steamapps\" # removes steam directory in home drive
-mkdir -p \"/shared/$USER/steamapps\" # makes user > steamapps in shared directory under user
-ln -sf \"/shared/$USER/steamapps\" \"/home/$USER/.local/share/Steam\" # links the shared folder to user local home
+rm -r "/home/$USER/.local/share/Steam/steamapps" # removes steam directory in home drive
+mkdir -p "/shared/$USER/steamapps" # makes user > steamapps in shared directory under user
+ln -sf "/shared/$USER/steamapps" "/home/$USER/.local/share/Steam" # links the shared folder to user local home
 
 ############### End Custom ###################
-",
+',
     subscribe => Package['steam'],
   }
-
-
-  # include wget
-  # include gdebi
-
-  # wget::fetch { 'steam.deb':
-  #   source      => 'https://steamcdn-a.akamaihd.net/client/installer/steam.deb',
-  #   destination => '/tmp/',
-  #   cache_dir   => '/var/cache/wget',
-  # } ~>
-  # package { 'steam':
-  #   ensure   => latest,
-  #   provider => gdebi,
-  #   source   => '/tmp/steam.deb',
-  # }
-
-  # package { 'steam':
-  #     # ensure => latest,
-  #     ensure => purged,
-  # }
-
-  # package { 'steamcmd':
-  #     # ensure => latest,
-  #     ensure => purged,
-  # }
-
-
-  # file { '/usr/bin/steam':
-  #   ensure  => file,
-   # 	owner	=> root,
-    # group	=> root,
-    # mode	=> '755',
-  #   source  => "puppet:///modules/profile/steam/steam",
-  #   subscribe => Package['steam']
-  # }
-  #
-  #
-  # # Remove the update notifier to prevent pop-up on all users.
-  # file { 'steam-update-notifier':
-  #   ensure => absent,
-  #   path => '/var/lib/update-notifier/user.d/steam-install-notify',
-  #   subscribe => Package['steam'],
-  # }
 
 }
