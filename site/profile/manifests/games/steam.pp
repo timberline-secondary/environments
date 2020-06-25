d95bf5b6cef1d3bb23cc0b6b7be637535fcedd1f
tylerecouture <tylerecouture@gmail.com>
2020-06-25 13:26:27 -0700

    fix steam install attempt99

--- site/profile/manifests/games/steam.pp
+++ site/profile/manifests/games/steam.pp
@@ -9,9 +9,14 @@ class profile::games::steam {
 
   exec { 'i386':
     command => '/usr/bin/dpkg --add-architecture i386',
-    unless  => '/bin/grep -q i386 /var/lib/dpkg/arch',
+    # unless  => '/bin/grep -q i386 /var/lib/dpkg/arch',
   }
   package { 'libnvidia-gl-440:i386':
+    ensure  => latest,
+    require => [Class['apt::update'], Exec['i386'], ]
+  }
+
+  package { 'libgl1-mesa-glx:i386':
     ensure  => latest,
     require => [Class['apt::update'], Exec['i386'], ]
   }
@@ -32,20 +37,15 @@ class profile::games::steam {
   # place this in the steam launch script at /usr/bin/steam right before the script exec's the launcher
   file_line { 'steamapps':
     path      => '/usr/games/steam',
-    match     => '^cd "\$LAUNCHSTEAMDIR"\n',
-    line      => 'cd "$LAUNCHSTEAMDIR"
+    after     => '# launch the Valve run script',
+    line      => '
 
-#############################################
-#
-# Custom (this modifies the install path of steam)
-#
-#############################################
-
+# CUSTOM VIA PUPPET - link steamapps (game install) dir to local /shared/ directory
 rm -r "/home/$USER/.local/share/Steam/steamapps" # removes steam directory in home drive
 mkdir -p "/shared/$USER/steamapps" # makes user > steamapps in shared directory under user
 ln -sf "/shared/$USER/steamapps" "/home/$USER/.local/share/Steam" # links the shared folder to user local home
 
-############### End Custom ###################
+
 ',
     subscribe => Package['steam'],
   }
