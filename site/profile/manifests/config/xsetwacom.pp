522bbcc35e66e5629745e460535a59ecf841c11e
tylerecouture <tylerecouture@gmail.com>
2021-01-02 15:53:03 -0800

    update wacom to include pro

--- site/profile/manifests/config/xsetwacom.pp
+++ site/profile/manifests/config/xsetwacom.pp
@@ -5,9 +5,15 @@ class profile::config::xsetwacom {
     owner   => 'root',
     group   => 'root',
     mode    => '0755',
-    content => '#!/bin/bash\n
-xsetwacom --set "Wacom Intuos5 touch M Pen stylus" MapToOutput 1920x1200+0+0\n
-xsetwacom --set "Wacom Intuos5 touch M Pen eraser" MapToOutput 1920x1200+0+0\n
+    content => '#!/bin/bash
+# Map Intuos5 Medium to single monitor and maintain aspect ratio (1200 instead of 1080)
+xsetwacom --set "Wacom Intuos5 touch M Pen stylus" MapToOutput 1920x1200+0+0
+xsetwacom --set "Wacom Intuos5 touch M Pen eraser" MapToOutput 1920x1200+0+0
+xsetwacom --set "Wacom Intuos5 touch M Pen cursor" MapToOutput 1920x1200+0+0
+# Intuos Pro Medium
+xsetwacom --set "Wacom Intuos Pro M Pen stylus" MapToOutput 1920x1200+0+0
+xsetwacom --set "Wacom Intuos Pro M Pen eraser" MapToOutput 1920x1200+0+0
+xsetwacom --set "Wacom Intuos Pro M Pen cursor" MapToOutput 1920x1200+0+0
 ',
   }
 
