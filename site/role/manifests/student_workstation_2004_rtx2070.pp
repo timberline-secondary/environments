40947dfb6a3eec2835f97789e20b2c45699aa66a
tylerecouture <tylerecouture@gmail.com>
2021-01-02 15:13:16 -0800

    remove node.js prfile to debug puppet runs

--- site/role/manifests/student_workstation_2004_rtx2070.pp
+++ site/role/manifests/student_workstation_2004_rtx2070.pp
@@ -51,7 +51,7 @@ class role::student_workstation_2004_rtx2070 {
   include profile::dev::python
   include profile::dev::vs_code
   # include profile::dev::docker
-  include profile::dev::nodejs
+  # include profile::dev::nodejs
   include profile::dev::vim
   # include profile::dev::processing
   # include profile::dev::virtual_machines
