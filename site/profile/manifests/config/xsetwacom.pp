ddf52dcf1cb562329712866f919a0253803ebc80
tylerecouture <tylerecouture@gmail.com>
2021-01-02 16:12:25 -0800

    update wacom to look for proper tablet

--- site/profile/manifests/config/xsetwacom.pp
+++ site/profile/manifests/config/xsetwacom.pp
@@ -8,13 +8,13 @@ class profile::config::xsetwacom {
     notify  => Reboot['after_run'],
     content => '#!/bin/bash
 # Map Intuos5 Medium to single monitor and maintain aspect ratio (1200 instead of 1080)
-if xsetwacom --list | grep -q 'Wacom Intuos5 touch M'; then
+if xsetwacom --list | grep -q \'Wacom Intuos5 touch M\'; then
   xsetwacom --set "Wacom Intuos5 touch M Pen stylus" MapToOutput 1920x1200+0+0 || true
   xsetwacom --set "Wacom Intuos5 touch M Pen eraser" MapToOutput 1920x1200+0+0 || true
   xsetwacom --set "Wacom Intuos5 touch M Pen cursor" MapToOutput 1920x1200+0+0 || true
 fi
 # Intuos Pro Medium
-if xsetwacom --list | grep -q 'Wacom Intuos5 Pro M'; then
+if xsetwacom --list | grep -q \'Wacom Intuos5 Pro M\'; then
   xsetwacom --set "Wacom Intuos Pro M Pen stylus" MapToOutput 1920x1200+0+0 || true
   xsetwacom --set "Wacom Intuos Pro M Pen eraser" MapToOutput 1920x1200+0+0 || true
   xsetwacom --set "Wacom Intuos Pro M Pen cursor" MapToOutput 1920x1200+0+0 || true
