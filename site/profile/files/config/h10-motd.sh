e9abb12d415c703ae055f699fee489336e022492
tylerecouture <tylerecouture@gmail.com>
2021-01-02 17:55:18 -0800

    add motd command

--- site/profile/files/config/h10-motd.sh
+++ site/profile/files/config/h10-motd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 RED='\033[1;31m'
 NC='\033[0m'
-echo "Thanks for checking the Hackerspace's Message Of The Day!  Here is today's message:"
+echo "Thanks for checking the Hackerspace's Message Of The Day!"
+echo "Here is today's message:"
 echo -e "> ${RED}Tetris is the GOAT.${NC}"
