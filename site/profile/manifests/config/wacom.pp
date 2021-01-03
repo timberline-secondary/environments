9cced07639b7abf7b56a89e6169f4eb781ac7fee
tylerecouture <tylerecouture@gmail.com>
2021-01-02 17:29:50 -0800

    fix wacom stuff

--- site/profile/manifests/config/wacom.pp
+++ site/profile/manifests/config/wacom.pp
@@ -16,12 +16,12 @@ class profile::config::wacom {
     group  => 'root',
     mode   => '0755',
     # notify  => Reboot['after_run'],
-    source => "puppet:///modules/${module_name}/h10-wacom.sh",
+    source => "puppet:///modules/profile/config/h10-wacom.sh",
   }
 
 
 systemd::unit_file { 'h10-wacom.service':
-  source => "puppet:///modules/${module_name}/h10-wacom.service",
+  source => "puppet:///modules/profile/config/h10-wacom.service",
 }
 ~> service {'h10-wacom':
   ensure => 'running',
