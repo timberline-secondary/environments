b48f5fd8d40880f8d88149773455f848604c72b9
tylerecouture <tylerecouture@gmail.com>
2021-04-22 13:22:13 -0700

    add nsupdate as cronjob

--- site/profile/manifests/config/dns.pp
+++ site/profile/manifests/config/dns.pp
@@ -6,6 +6,7 @@ class profile::config::dns {
   $dns_server_ip = '192.168.66.40'
   $fqdn = $facts['networking']['fqdn']
   $ip = $facts['networking']['ip']
+
   file { '/etc/nsupdate':
     ensure  => 'present',
     content => "
@@ -22,4 +23,19 @@ send
     provider => 'shell',
     require  => File['/etc/nsupdate'],
   }
+
+  # The cronw ill keep running even if puppet runs are failing, this will keep the 
+  # workstation on the network for a bit longer at least, but puppet is still required to update the 
+  # /etc/nsupdate file to ensure it has the wokrstation's current IP, in case it hase changed
+  cron { 'nsupdate':
+    command => '/usr/sbin/nsupdate /etc/nsupdate',
+    user    => 'root',
+    minute  => 0,  # once per hour on minute 0
+    require => File['/etc/nsupdate']
+  }
+
+
+
 }
+
+
