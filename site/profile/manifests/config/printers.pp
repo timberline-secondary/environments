e77f5fa0aa72c1611b5718325c901d7c91ce6a91
tylerecouture <tylerecouture@gmail.com>
2021-11-29 14:49:09 -0800

    remove manual printer install (use Hightower CUPS server only)

--- site/profile/manifests/config/printers.pp
+++ site/profile/manifests/config/printers.pp
@@ -6,17 +6,23 @@ class profile::config::printers {
     purge_unmanaged_queues => true,
   }
 
-  # Using a suitable model from the output of the command lpinfo -m on the node
-  cups_queue { 'Hackerspace_BW_Printer_LexmarkT644':
-    ensure    => 'printer',
-    model     => 'foomatic-db-compressed-ppds:0/ppd/foomatic-ppd/Lexmark-T644-Postscript.ppd',
-    uri       => 'socket://192.168.43.101', # Replace with your printer's URI
-    accepting => true,
-    enabled   => true,
-    options   => {
-      'Duplex'   => 'DuplexNoTumble',
-    }
-  }
+  ###
+  # With CUPS installed, computers should be getting printer from Hightower (cups server):
+  # http://hightower:631/printers/
+  #
+
+
+  # # Using a suitable model from the output of the command lpinfo -m on the node
+  # cups_queue { 'Hackerspace_BW_Printer_LexmarkT644':
+  #   ensure    => 'printer',
+  #   model     => 'foomatic-db-compressed-ppds:0/ppd/foomatic-ppd/Lexmark-T644-Postscript.ppd',
+  #   uri       => 'socket://192.168.43.101', # Replace with your printer's URI
+  #   accepting => true,
+  #   enabled   => true,
+  #   options   => {
+  #     'Duplex'   => 'DuplexNoTumble',
+  #   }
+  # }
 
   # Using a suitable model from the output of the command lpinfo -m on the node
   # cups_queue { 'Hackerspace_Color_Printer_HPColorLaserJet5550':
