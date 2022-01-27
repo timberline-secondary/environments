52eb5ddd63515629b098a47367b19f13cc9160a5
tylerecouture <tylerecouture@gmail.com>
2022-01-27 09:36:37 -0800

    nsupdate every 5th minute

--- site/profile/manifests/config/dns.pp
+++ site/profile/manifests/config/dns.pp
@@ -17,34 +17,37 @@ class profile::config::dns {
   # Needs to run during install too, to make reimaging easier (puppet certificates)
   # See late-command /var/www/html/preseeds/20.04/user-data on lannister
 
-  file { '/etc/nsupdate':
-    ensure  => 'present',
-    content => "
-server ${$dns_server_ip}
-update delete ${fqdn} A
-update add ${fqdn} 86400 A ${ip}
-send
-"
-  }
+#   file { '/etc/nsupdate':
+#     ensure  => 'present',
+#     content => "
+# server ${$dns_server_ip}
+# update delete ${fqdn} A
+# update add ${fqdn} 86400 A ${ip}
+# send
+# "
+#   }
+
+  $command = "echo \"server${dns_server_ip}\n \
+  update delete $(/usr/bin/hostname).hackerspace.tbl A\n \
+  update add $(/usr/bin/hostname).hackerspace.tbl 86400 A $(/usr/bin/ip address show up | grep -Pwo '192\.168\.43\.[0-9]{1,3}')\n \
+  send\n"
 
   exec { 'nsupdate':
-    path     => ['/bin', '/usr/bin'],
-    command  => 'nsupdate /etc/nsupdate',
+    # path     => ['/bin', '/usr/bin'],  # all paths fully qualified so not needed
+    command  => "${command} | /usr/bin/nsupdate",
     provider => 'shell',
-    require  => File['/etc/nsupdate'],
+    # require  => File['/etc/nsupdate'],
   }
 
   # The cronw ill keep running even if puppet runs are failing, this will keep the 
   # workstation on the network for a bit longer at least, but puppet is still required to update the 
   # /etc/nsupdate file to ensure it has the wokrstation's current IP, in case it hase changed
   cron { 'nsupdate':
-    command => '/usr/bin/nsupdate /etc/nsupdate',
+    command => "${command} | /usr/bin/nsupdate",
     user    => 'root',
-    minute  => 0,  # once per hour on minute 0
-    require => File['/etc/nsupdate']
+    minute  => '*/5',  # every 5 mins
+    # require => File['/etc/nsupdate']
   }
-
-
 
 }
 
