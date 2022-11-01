5f7bdd4951f7b0cb5fb56ec0f1fd754ecf6beb56
tylerecouture <tylerecouture@gmail.com>
2022-11-01 14:31:09 -0700

    kernel 5.4.0

--- site/profile/manifests/config/grub.pp
+++ site/profile/manifests/config/grub.pp
@@ -2,7 +2,7 @@
 class profile::config::grub {
 
   # Kernel Update to 6.0 is causing boot problems, need to pin kernel
-  $kernel_id = '5.19.0-17.2-liquorix-amd64'
+  $kernel_id = '5.4.0-128-generic'
   $grub_menu_kernel = "Advanced options for Ubuntu>Ubuntu, with Linux ${kernel_id}"
   $kernel_image = "linux-image-${kernel_id}"
   $kernel_headers = "linux-headers-${kernel_id}"
