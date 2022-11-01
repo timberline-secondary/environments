364298134d7485be9e3076eda1f59b4558662e4c
tylerecouture <tylerecouture@gmail.com>
2022-11-01 12:36:00 -0700

    try grub

---
+++ site/profile/manifests/config/grub.pp
@@ -0,0 +1,27 @@
+#
+class profile::config::grub {
+
+  # Kernel Update to 6.0 is causing boot problems, need to pin kernel
+  $kernel_id = '5.19.0-17.2-liquorix-amd64'
+  $grub_menu_kernel = "Advanced options for Ubuntu>Ubuntu, with Linux ${kernel_id}"
+  $kernel_image = "linux-image-${kernel_id}"
+  $kernel_headers = "linux-headers-${kernel_id}"
+
+  file_line { '/etc/default/grub':
+    line   => "GRUB_DEFAULT=\"${grub_menu_kernel}\"",
+    match  => '^GRUB_DEFAULT',
+    notify => Exec['update_grub', 'pin_kernel'],
+  }
+
+  # don't let this working kernel be deleted!
+  exec { 'pin_kernel':
+    command     => "/usr/bin/apt-mark hold ${kernel_image} ${kernel_headers}",
+    refreshonly => true,
+  }
+
+  exec { 'update_grub' :
+    command     => '/usr/sbin/update-grub',
+    refreshonly => true,
+  }
+
+}
