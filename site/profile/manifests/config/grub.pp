6d4da41fe3456f4977b5dc957d135cfb282dbedb
tylerecouture <tylerecouture@gmail.com>
2022-11-02 10:02:36 -0700

    kernel 5.4.0-131

--- site/profile/manifests/config/grub.pp
+++ site/profile/manifests/config/grub.pp
@@ -2,16 +2,21 @@
 class profile::config::grub {
 
   # Kernel Update to 6.0 is causing boot problems, need to pin kernel
-  $kernel_id = '5.4.0-128-generic'
+  $kernel_id = '5.4.0-131-generic'
   $grub_menu_kernel = "Advanced options for Ubuntu>Ubuntu, with Linux ${kernel_id}"
   $kernel_image = "linux-image-${kernel_id}"
   $kernel_headers = "linux-headers-${kernel_id}"
 
+  package {$kernel_image:
+    ensure  => latest,
+  }
+
   file_line { '/etc/default/grub':
-    path   => '/etc/default/grub',
-    line   => "GRUB_DEFAULT=\"${grub_menu_kernel}\"",
-    match  => '^GRUB_DEFAULT',
-    notify => Exec['update_grub', 'pin_kernel'],
+    path      => '/etc/default/grub',
+    line      => "GRUB_DEFAULT=\"${grub_menu_kernel}\"",
+    match     => '^GRUB_DEFAULT',
+    subscribe => Package[$kernel_image],
+    notify    => Exec['update_grub', 'pin_kernel'],
   }
 
   # don't let this working kernel be deleted!
