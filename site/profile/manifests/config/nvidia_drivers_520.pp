180f214778fbf536f54486c0af2b357e6f7cbbf3
tylerecouture <tylerecouture@gmail.com>
2022-11-15 09:14:29 -0800

    update nvidia 520

--- site/profile/manifests/config/nvidia_drivers_520.pp
+++ site/profile/manifests/config/nvidia_drivers_520.pp
@@ -1,21 +1,21 @@
 class profile::config::nvidia_drivers_520 {
 
-  package { 'dkms':
-    ensure => latest,
-  }
+  # package { 'dkms':
+  #   ensure => latest,
+  # }
 
   package { 'nvidia-driver-520':
     ensure => latest,
   }
 
-  package { 'nvidia-dkms-520':
-    ensure => latest,
-  }
+  # package { 'nvidia-dkms-520':
+  #   ensure => latest,
+  # }
 
-  package { 'nvidia-cuda-toolkit':
-    ensure  => latest,
-    require => Package['nvidia-driver-420'],
-    # notify  => Reboot['after_run'],
-  }
+  # package { 'nvidia-cuda-toolkit':
+  #   ensure  => latest,
+  #   require => Package['nvidia-driver-420'],
+  #   # notify  => Reboot['after_run'],
+  # }
 
 }
