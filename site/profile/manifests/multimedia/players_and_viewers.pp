391a4a16c89c174c209d1ec5046c8efe27de8f52
tylerecouture <tylerecouture@gmail.com>
2022-11-17 17:01:58 -0800

    remove spotify and kaku (problems with ppa and keys etc)

--- site/profile/manifests/multimedia/players_and_viewers.pp
+++ site/profile/manifests/multimedia/players_and_viewers.pp
@@ -21,21 +21,21 @@ class profile::multimedia::players_and_viewers {
   #
   #############
 
-  include gdebi
+  # include gdebi
 
-  $kaku_version = '2.0.2'
+  # $kaku_version = '2.0.2'
 
-  archive{ '/tmp/Kaku.deb':
-    ensure => present,
-    source => "https://github.com/EragonJ/Kaku/releases/download/${kaku_version}/Kaku_${kaku_version}_amd64.deb",
-  }
+  # archive{ '/tmp/Kaku.deb':
+  #   ensure => present,
+  #   source => "https://github.com/EragonJ/Kaku/releases/download/${kaku_version}/Kaku_${kaku_version}_amd64.deb",
+  # }
 
-  package { 'kaku':
-    ensure    => purged,
-    provider  => gdebi,
-    source    => '/tmp/Kaku.deb',
-    subscribe => Archive['/tmp/Kaku.deb']
-  }
+  # package { 'kaku':
+  #   ensure    => purged,
+  #   provider  => gdebi,
+  #   source    => '/tmp/Kaku.deb',
+  #   subscribe => Archive['/tmp/Kaku.deb']
+  # }
 
   # ###########
   #
@@ -45,23 +45,23 @@ class profile::multimedia::players_and_viewers {
   # ############
 
 
-  include apt
+  # include apt
 
-  # manually add public key to /etc/apt/trusted.gpg.d/ directory
+  # # manually add public key to /etc/apt/trusted.gpg.d/ directory
 
-  apt::source { 'spotify-stable-repo2':
-    location => 'http://repository.spotify.com',
-    release  => 'stable',
-    repos    => 'non-free',
-    key      => {
-      id     => '8FD3D9A8D3800305A9FFF259D1742AD60D811D58',
-      # id     => 'D1742AD60D811D58',
-      server => 'hkp://keyserver.ubuntu.com:80',
-    },
-  }
-  package { 'spotify-client':
-    ensure  => purged,
-    require => [ Class['apt::update'], Apt::Source['spotify-stable-repo2'] ],
-  }
+  # apt::source { 'spotify-stable-repo2':
+  #   location => 'http://repository.spotify.com',
+  #   release  => 'stable',
+  #   repos    => 'non-free',
+  #   key      => {
+  #     id     => '8FD3D9A8D3800305A9FFF259D1742AD60D811D58',
+  #     # id     => 'D1742AD60D811D58',
+  #     server => 'hkp://keyserver.ubuntu.com:80',
+  #   },
+  # }
+  # package { 'spotify-client':
+  #   ensure  => purged,
+  #   require => [ Class['apt::update'], Apt::Source['spotify-stable-repo2'] ],
+  # }
 
 }
